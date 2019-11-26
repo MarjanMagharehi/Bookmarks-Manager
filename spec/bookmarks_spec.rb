07835db07feede0d133d461a1d28e1a05a569fdb
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-26 16:14:37 +0000

    updated the bookmarks unit test, to include a url from the database we created

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,6 +2,6 @@ require 'bookmarks'
 
 describe Bookmarks do
   it 'has a hardcoded list of bookmarks' do
-    expect(Bookmarks.all).to include("https://sqlzoo.net/wiki/SELECT_basics")
+    expect(Bookmarks.all).to include("http://www.google.com")
   end
 end
