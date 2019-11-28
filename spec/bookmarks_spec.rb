0c72f928c004df32762add3196bc6d0d70cf9928
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-28 14:59:40 +0000

    Refactored my returns all bookmarks test to make it neater

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,11 +2,15 @@ require 'bookmarks'
 
 describe Bookmarks do
   it 'returns a list of bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-    bookmarks = Bookmarks.all # could be bookmarklist?
+    Bookmarks.create(url: 'http://www.makersacademy.com')
+    Bookmarks.create(url: 'http://www.destroyallsoftware.com')
+    Bookmarks.create(url: 'http://www.google.com')
+    bookmarks = Bookmarks.all
+    # connection = PG.connect(dbname: 'bookmark_manager_test')
+    # connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    # could be bookmarklist?
     # expect(Bookmarks.all).to include("http://google.com")
     expect(bookmarks).to include('http://www.makersacademy.com')
     expect(bookmarks).to include('http://www.destroyallsoftware.com')
