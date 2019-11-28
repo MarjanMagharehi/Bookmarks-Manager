b978e0d336039df3142b89a3b977af232e5212ad
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-28 16:32:08 +0000

    Passed failing unit tests, for adding a title to the bookmarks

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -4,7 +4,7 @@ class Bookmarks
   attr_reader :title, :url
   def initialize(title, url)
     @title = title
-    @url = url 
+    @url = url
   end
 
   def self.all
@@ -16,13 +16,13 @@ class Bookmarks
     result = connection.exec("SELECT * FROM bookmarks")
     result.map {|bookmark| bookmark['url']}
   end
-  def self.create(url:)
+  def self.create(url:, title:)
     if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')")
   end
 
 end
